1b39f3555b0c71826b99adb18d32d00c0480eb6b
Alana Nardes <alana.nardes.dev@gmail.com>
2025-07-19 21:27:54 +0000

    Arrays - Matriz e Vetores no Jogo de Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // Construindo o Jogo de Batalha Naval
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Vetor do Tipo char para declarar e inicializar a identificação com letras, representando as Colunas do Tabuleiro.
+    int tabuleiro[10][10]; // Declaração da Matriz do tipo inteiro, de tamanho 10x10 [linha]x[coluna], representando o tabuleiro.
+    int navioHorizontal[3] = {3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o Navio em posição Horizontal, que ocupará 3 posições em uma linha do Tabuleiro.
+    int navioVertical[4]= {3, 3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o Navio em posição Vertical, que ocupará 4 posições em uma coluna do Tabuleiro.
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    for (int i = 0; i < 10; i++) { // Loop Aninhado (for-for) para inicializar os índices da Matriz de linha[i] e coluna[j] com valor 0, representando a água no Tabuleiro.
+        for (int j = 0; j < 10; j++) {
+            tabuleiro[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < 3; i++) { // Loop for para posicionar o Navio Horizontal nos índices [5][5], [5][6] e [5][7], ocupando 3 posições na linha 6 x F do Tabuleiro.
+        tabuleiro[5][5 + i] = navioHorizontal[i];
+    }
+
+    for (int i = 0; i < 4; i++) { // Loop for para posicionar o Navio Vertical nos índices [6][2], [7][2], [8][2] e [9][2], ocupando 4 posições na coluna C x 7 do Tabuleiro.
+        tabuleiro[6 + i][2] = navioVertical[i];
+    }
+
+    printf("\n\n*** TABULEIRO BATALHA NAVAL ***\n\n");
+
+    printf("   "); // Espaço impresso apena para o alinhamento das Letras x Colunas do Tabuleiro.
+    for (int j = 0; j < 10; j++) { // Loop for para a impressão do Vetor de identificação com letras, representando as Colunas do Tabuleiro.
+        printf(" %c", linha[j]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) { // Estrutura externa do Loop Aninhado (for-for), para a impressão da identicação das linhas de 1 à 10, do lado esquerdo do Tabuleiro.
+        printf("%2d ", i + 1); // "%2d" - Insere um pequeno espaço na frente dos numeros, para alinhar a Coluna de identificação Númerica do Tabuleiro. "i + 1" - Começa a sequencia de impressão a partir do nº 1 e não pelo nº 0.
+        for (int j = 0; j < 10; j++) { // Estrutura interna do Loop Aninhado (for-for), para a impressão da Matriz [i=10] x [j=10], representando o Tabuleiro.
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n"); // Impressão apenas para quebra de linha a cada Loop. 
+    }
+
+    printf("\n");
 
     return 0;
 }
