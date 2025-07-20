5e9d1ea2d35492b4c07ffb344b256b5030ae66a2
Alana Nardes <alana.nardes.dev@gmail.com>
2025-07-20 17:49:10 +0000

    Imprementação 2 loops for para Diagonal da Matriz

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -7,8 +7,9 @@ int main() {
     char linha[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; // Vetor do Tipo char para declarar e inicializar a identificação com letras, representando as Colunas do Tabuleiro.
     int tabuleiro[10][10]; // Declaração da Matriz do tipo inteiro, de tamanho 10x10 [linha]x[coluna], representando o tabuleiro.
     int navioHorizontal[3] = {3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o Navio em posição Horizontal, que ocupará 3 posições em uma linha do Tabuleiro.
-    int navioVertical[4]= {3, 3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o Navio em posição Vertical, que ocupará 4 posições em uma coluna do Tabuleiro.
-    
+    int navioVertical[4] = {3, 3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o Navio em posição Vertical, que ocupará 4 posições em uma coluna do Tabuleiro.
+    int navioDiagonal1[3] = {3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o 1º Navio em posição Diagonal Principal, que ocupará 3 posições em uma diagonal do Tabuleiro.
+    int navioDiagonal2[3] = {3, 3, 3}; // Declaração e inicialização do Vetor de tipo inteiro para implementar o 2º Navio em posição Diagonal Secundária, que ocupará 3 posições em uma diagonal do Tabuleiro.
 
     for (int i = 0; i < 10; i++) { // Loop Aninhado (for-for) para inicializar os índices da Matriz de linha[i] e coluna[j] com valor 0, representando a água no Tabuleiro.
         for (int j = 0; j < 10; j++) {
@@ -16,12 +17,20 @@ int main() {
         }
     }
 
-    for (int i = 0; i < 3; i++) { // Loop for para posicionar o Navio Horizontal nos índices [5][5], [5][6] e [5][7], ocupando 3 posições na linha 6 x F do Tabuleiro.
-        tabuleiro[5][5 + i] = navioHorizontal[i];
+    for (int i = 0; i < 3; i++) { // Loop for para posicionar o Navio Horizontal nos índices [8][5], [8][6] e [8][7], ocupando 3 posições na horizontal a partir da coordenada 9 x F do Tabuleiro.
+        tabuleiro[8][5 + i] = navioHorizontal[i];
     }
 
-    for (int i = 0; i < 4; i++) { // Loop for para posicionar o Navio Vertical nos índices [6][2], [7][2], [8][2] e [9][2], ocupando 4 posições na coluna C x 7 do Tabuleiro.
+    for (int i = 0; i < 4; i++) { // Loop for para posicionar o Navio Vertical nos índices [6][2], [7][2], [8][2] e [9][2], ocupando 4 posições na vertical a partir da coordenada C x 7 do Tabuleiro.
         tabuleiro[6 + i][2] = navioVertical[i];
+    }
+
+    for (int i = 0; i < 3; i++) { // Loop for para posicionar o 1º Navio Diagonal nos índices [1][2], [2][3] e [3][4], ocupando 3 posições na diagonal a partir da coordenada 2 x C do Tabuleiro.
+        tabuleiro[1 + i][2 + i] = navioDiagonal1[i];
+    }
+
+    for (int i = 0; i < 3; i++){ // Loop for para posicionar o 2º Navio Diagonal(Secundária) nos índices [3][8], [4][7] e [5][6], ocupando 3 posições na diagonal inversa a partir da coordenada 4 x I do Tabuleiro.
+        tabuleiro[3 + i][8 - i] = navioDiagonal2[i];
     }
 
     printf("\n\n*** TABULEIRO BATALHA NAVAL ***\n\n");
@@ -34,7 +43,7 @@ int main() {
     printf("\n");
 
     for (int i = 0; i < 10; i++) { // Estrutura externa do Loop Aninhado (for-for), para a impressão da identicação das linhas de 1 à 10, do lado esquerdo do Tabuleiro.
-        printf("%2d ", i + 1); // "%2d" - Insere um pequeno espaço na frente dos numeros, para alinhar a Coluna de identificação Númerica do Tabuleiro. "i + 1" - Começa a sequencia de impressão a partir do nº 1 e não pelo nº 0.
+        printf("%2d ", i + 1); // "%2d" - Encaixa os elementos de um numero na 2º casa decimal, para alinhar a Coluna de identificação Númerica do Tabuleiro. "i + 1" - Começa a sequencia de impressão a partir do nº 1 e não pelo nº 0.
         for (int j = 0; j < 10; j++) { // Estrutura interna do Loop Aninhado (for-for), para a impressão da Matriz [i=10] x [j=10], representando o Tabuleiro.
             printf(" %d", tabuleiro[i][j]);
         }
